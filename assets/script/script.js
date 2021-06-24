4a2caa60b49623e3ea825e6f48d4c455df7b00c5
LeoDarpan <leo29791@gmail.com>
2021-06-24 15:52:46 +0530

    Responsive Slider

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,10 @@
 var left = document.querySelectorAll(".left");
+var left1 = document.querySelectorAll(".left1");
+var left2 = document.querySelectorAll(".left2");
 var slider = document.querySelector(".slider");
 var right = document.querySelectorAll(".right");
+var right1 = document.querySelectorAll(".right1");
+var right2 = document.querySelectorAll(".right2");
 var header = document.getElementsByTagName("header");
 var main = document.querySelector(".main");
 var offset = 0;
@@ -13,9 +17,38 @@ left[0].addEventListener("click", ()=>{
     translateX1(offset);
 });
 
+left1[0].addEventListener("click", ()=>{
+    offset += 170;
+    if(offset > 0)
+        offset = 0;
+    translateX1(offset);
+});
+
+left2[0].addEventListener("click", ()=>{
+    offset += 170;
+    if(offset > 0)
+        offset = 0;
+    translateX1(offset);
+});
+
+
 right[0].addEventListener("click", ()=>{
     offset -= 170;
     if(offset <= -850)
+        offset = 0;
+    translateX1(offset);
+});
+
+right1[0].addEventListener("click", ()=>{
+    offset -= 170;
+    if(offset <= -1190)
+        offset = 0;
+    translateX1(offset);
+});
+
+right2[0].addEventListener("click", ()=>{
+    offset -= 170;
+    if(offset <= -1020)
         offset = 0;
     translateX1(offset);
 });
